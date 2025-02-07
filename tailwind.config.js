b34fb799f711a807dc1a9b159100378658202edd
sehwan505 <sehwan505@gmail.com>
2025-02-07 17:00:42 +0900

    Feat: add configuration for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/webview/**/*.{html,ts}'],
+  content: ['./src/webview/**/*.{ts,tsx,html}'],
   theme: {
     extend: {
       colors: {
