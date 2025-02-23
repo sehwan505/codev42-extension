bc3aa1629a827b06f6cd4aea5c0010cf9791e541
sehwan505 <sehwan505@gmail.com>
2025-02-23 23:24:31 +0900

    Chore: react build option for fixing output file name

--- webview/vite.config.ts
+++ webview/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: './',
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    }
+});
